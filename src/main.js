b54ea3fb23f32753d64625c4ad76e463cd2eed46
cbcook <cbcook321@gmail.com>
2019-01-15 21:42:07 -0500

    Now changes color of third item to red

--- src/main.js
+++ src/main.js
@@ -9,6 +9,16 @@ let addItem = () => {
     let newItem = document.createElement('li');
     newItem.innerHTML = input;
 
+    let currentItems = document.getElementById('listDisplay').getElementsByTagName('li');
+    let itemCount = currentItems.length;
+
+    if((itemCount + 1) % 3 === 0){
+        newItem.classList.add('redItem');
+    }else{
+
+    }
     list.appendChild(newItem);
+
+
 
 }
